2125387768f411f2fb60cc33faece5e1a339dda2
paulbis <tfydRuB2QCh33yH>
2021-05-23 21:33:38 +0200

    modifications diverses

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -17,18 +17,22 @@ public class AnalyticsCounter {
 		System.out.println("Lancement du programme... "); 
 		
 		final ReadSymptomDataFromFile iSr = new ReadSymptomDataFromFile("symptoms.txt");
-		System.out.println("Récupération du fichier OK .... ");
-		final List<String> symptoms = iSr.getSymptoms();
-		final SymptomWritter writer = new SymptomWritter(); 
 		
+		System.out.println("Récupération du fichier OK .... ");
+		
+		final List<String> symptoms = iSr.getSymptoms();
+		
+		final SymptomWritter writer = new SymptomWritter(); 
 		CounterSymptoms counterSymptoms = new CounterSymptoms();
 		counterSymptoms.count(symptoms);
+		
 		Map<String, Integer> testMap = counterSymptoms.getMyMap(); 
+		
 		//writer.writeConsole(counterSymptoms.getMyMap());
 		//writer.writeFile(counterSymptoms.getMyMap());
+		
 		SortSymptoms sortSymptoms = new SortSymptoms(); 
 		sortSymptoms.sortMap(testMap);
-		
 		writer.writeFile(sortSymptoms.getMapSorted());
 		
 	}
